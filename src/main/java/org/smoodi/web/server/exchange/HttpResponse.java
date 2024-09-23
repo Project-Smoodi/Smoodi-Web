468bd472ebd12dc5ae739820a5c97eda18f9708d
Daybreak312 <ty82afg12@gmail.com>
2024-09-23 22:11:09 +0900

    REFACT :: Response

--- src/main/java/org/smoodi/web/server/exchange/HttpResponse.java
+++ src/main/java/org/smoodi/web/server/exchange/HttpResponse.java
@@ -2,15 +2,42 @@ package org.smoodi.web.server.exchange;
 
 public interface HttpResponse extends Response {
 
-    @Override
-    int getPort();
-
-    @Override
-    String getAddress();
-
+    /**
+     * <p>Return the content of response.</p>
+     *
+     * <p>Depending on the {@code Content-Type} in {@link HttpResponse#getHeaders() http headers}, it can be cast as a specific type.</p>
+     *
+     * <p>Like this:</p>
+     * <table>
+     *     <thead>
+     *         <td><b>Content-Type</b></td>
+     *         <td><b>Java Type</b></td>
+     *     </thead>
+     *     <tr>
+     *         <td>application/json</td>
+     *         <td>Object</td>
+     *     </tr>
+     *     <tr>
+     *         <td>text/html</td>
+     *         <td>String</td>
+     *     </tr>
+     *     <tr>
+     *         <td>text/plain</td>
+     *         <td>String</td>
+     *     </tr>
+     * </table>
+     *
+     * @return The content of response.
+     */
     @Override
     Object getContent();
 
+    /**
+     * <p>Return headers of response.</p>
+     *
+     * @return Headers of response.
+     * @see HttpHeaders
+     */
     @Override
     HttpHeaders getHeaders();
 }
