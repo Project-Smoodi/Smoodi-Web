487f933f17beaf525fb477d282bb752fa4e9767e
Daybreak312 <ty82afg12@gmail.com>
2024-09-23 22:05:23 +0900

    REFACT :: Request

--- src/main/java/org/smoodi/web/server/exchange/HttpRequest.java
+++ src/main/java/org/smoodi/web/server/exchange/HttpRequest.java
@@ -2,15 +2,91 @@ package org.smoodi.web.server.exchange;
 
 public interface HttpRequest extends Request {
 
-    @Override
-    int getPort();
+    /**
+     * <p>Return the request URL.</p>
+     *
+     * @return The request URL. ex) {@code http://loaclhost:8080/home?key=value}
+     */
+    String getUrl();
 
-    @Override
-    String getAddress();
+    /**
+     * <p>Return the host.</p>
+     *
+     * <p>It's part of {@link HttpRequest#getUrl()}</p>
+     *
+     * @return The host. ex) When {@link HttpRequest#getUrl()} is {@code http://loaclhost:8080/home?key=value}, then {@code localhost:8080}
+     */
+    String getHost();
 
+    /**
+     * <p>Return the request URI.</p>
+     *
+     * <p>It's part of {@link HttpRequest#getUrl()}</p>
+     *
+     * @return The request URI. ex) When {@link HttpRequest#getUrl()} is {@code http://loaclhost:8080/home?key=value}, then {@code localhost:8080/home}
+     */
+    String getUri();
+
+    /**
+     * <p>Return is it secured protocol.</p>
+     *
+     * @return {@code true} If the protocol is HTTPS. {@code false} If the protocol is HTTP.
+     */
+    boolean isSecureProtocol();
+
+    /**
+     * <p>Return the content of request.</p>
+     *
+     * <p>Depending on the {@code Content-Type(Http Header)}, it can be cast as a specific type.</p>
+     *
+     * <p>Like this:</p>
+     * <table>
+     *     <thead>
+     *         <td><b>Content-Type</b></td>
+     *         <td><b>Java Type</b></td>
+     *     </thead>
+     *     <tr>
+     *         <td>application/json</td>
+     *         <td>Object</td>
+     *     </tr>
+     *     <tr>
+     *         <td>text/html</td>
+     *         <td>String</td>
+     *     </tr>
+     *     <tr>
+     *         <td>text/plain</td>
+     *         <td>String</td>
+     *     </tr>
+     * </table>
+     *
+     * @return The content of request.
+     */
     @Override
     Object getContent();
 
+    /**
+     * <p>Return headers of request.</p>
+     *
+     * @return Headers of request.
+     * @see HttpHeaders
+     */
     @Override
     HttpHeaders getHeaders();
+
+    /**
+     * <p>Return the path of request.</p>
+     *
+     * <p>It's part of URL.</p>
+     *
+     * @return The path of request. ex) When {@link HttpRequest#getUrl()} is {@code http://localhost:8080/home?key=value}, then {@code /home}
+     */
+    String getPath();
+
+    /**
+     * <p>Return the http method of request.</p>
+     *
+     * @return The http method of request.
+     * @see HttpMethod
+     */
+    HttpMethod getMethod();
 }
--- src/main/java/org/smoodi/web/server/exchange/Request.java
+++ src/main/java/org/smoodi/web/server/exchange/Request.java
@@ -2,11 +2,25 @@ package org.smoodi.web.server.exchange;
 
 public interface Request {
 
-    int getPort();
+    /**
+     * <p>Return the IP Address of Client.</p>
+     *
+     * @return The IP Address of client. ex) {@code 127.0.0.1}
+     */
+    String getIpAddress();
 
-    String getAddress();
-
+    /**
+     * <p>Return the content of request.</p>
+     *
+     * @return The content of request.
+     */
     Object getContent();
 
+    /**
+     * <p>Return headers of request.</p>
+     *
+     * @return Headers of request.
+     * @see Headers
+     */
     Headers getHeaders();
 }
