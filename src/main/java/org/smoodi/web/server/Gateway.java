eb68316ad29754e35913d3ac952f21b2ae326d5a
Daybreak312 <ty82afg12@gmail.com>
2025-05-08 14:15:59 +0900

    REFACT :: IOStream Gateway 제거, 단순화

--- src/main/java/org/smoodi/web/server/Gateway.java
+++ src/main/java/org/smoodi/web/server/Gateway.java
@@ -1,19 +1,11 @@
 package org.smoodi.web.server;
 
 import org.smoodi.annotation.NotNull;
-import org.smoodi.web.router.Router;
+import org.smoodi.physalus.transfer.Request;
+import org.smoodi.physalus.transfer.Response;
 
-import java.io.InputStream;
-import java.io.OutputStream;
-
-/**
- * <p>서버의 최전단에서 클라이언트 요청 및 {@link Router} 응답의 가공과 처리 가능한 {@link Router}를 연결하는 역할을 수행</p>
- *
- * <p>1. 클라이언트의 요청을 <i>Smoodi Web</i>에서 사용하는 형태인 {@link org.smoodi.physalus.transfer.Request Request}로 가공하고 {@link Router}에게 전달</p>
- * <p>2. {@link Router}가 넘겨준 {@link org.smoodi.physalus.transfer.Response Response} 응답 객체를 클라이언트가 분석 가능한 표준 형태로 가공</p>
- */
 public interface Gateway {
 
     @NotNull
-    OutputStream service(@NotNull InputStream inputStream, @NotNull OutputStream outputStream);
+    Response service(@NotNull Request request, @NotNull Response response);
 }
